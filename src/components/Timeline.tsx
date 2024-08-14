308be74edddb1ad0e6d3082c09285e12af26e9e4
AshkanArabim <ashkan.arabim@gmail.com>
2024-08-13 20:28:55 -0600

    make timeline dynamic

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,20 +1,30 @@
 import * as React from "react";
 import TimelineEntry from "./TimelineEntry";
 
-const EntriesCol = () => {
-	return (
-    <div className="grow">
-      <h1 className="text-3xl p-4">Timeline</h1>
-      <TimelineEntry />
-      <TimelineEntry />
-      <TimelineEntry />
-      <TimelineEntry />
-      <TimelineEntry />
-      <TimelineEntry />
-      <TimelineEntry />
-      <TimelineEntry />
-    </div>
-  );
-};
+interface Data {
+	allMarkdownRemark: {
+		nodes: [
+			{
+				html: string;
+				frontmatter: {
+					date: string;
+					slug: string;
+					title: string;
+				};
+			}
+		];
+	};
+}
 
-export default EntriesCol;
+export default function EntriesCol({ data }: { data: Data }) {
+	return (
+		<div className="grow">
+			<h1 className="text-3xl p-4">Timeline (red means clickable)</h1>
+			{data.allMarkdownRemark.nodes.map(
+				({ html, frontmatter: { date, slug, title } }) => (
+					<TimelineEntry date={date} title={title} slug={slug} peek={html} />
+				)
+			)}
+		</div>
+	);
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,50 @@
 import * as React from "react";
-import type { HeadFC, PageProps } from "gatsby";
+import type { HeadFC } from "gatsby";
 import TitleBar from "../components/TitleBar";
 import ContactsCol from "../components/ContactsCol";
-import EntriesCol from "../components/Timeline";
+import Timeline from "../components/Timeline";
+import { graphql } from "gatsby";
 
-const IndexPage: React.FC<PageProps> = () => {
+interface Data {
+	allMarkdownRemark: {
+		nodes: [
+			{
+				html: string;
+				frontmatter: {
+					date: string;
+					slug: string;
+					title: string;
+				};
+			}
+		];
+	};
+}
+
+export default function IndexPage({ data }: { data: Data }) {
 	return (
 		<div className="flex flex-col items-center min-h-screen">
 			<TitleBar />
 			<div className="max-w-6xl w-full grow flex">
 				<ContactsCol />
-				<EntriesCol />
+				<Timeline data={data} />
 			</div>
 		</div>
 	);
-};
-
-export default IndexPage;
+}
 
 export const Head: HeadFC = () => <title>blog.ashkan.zone</title>;
+
+export const query = graphql`
+	{
+		allMarkdownRemark {
+			nodes {
+				html
+				frontmatter {
+					date(formatString: "YYYY-MM-DD")
+					slug
+					title
+				}
+			}
+		}
+	}
+`;
