18dcbd089f09c0d1d81e0393a96f093b8f278c83
AshkanArabim <ashkan.arabim@gmail.com>
2024-08-15 21:36:54 -0600

    fix li styling

--- src/components/BlogBody.tsx
+++ src/components/BlogBody.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const BlogBody = ({ children }: Props) => {
 	return (
-		<div className="grow p-4 [&_p]:mb-10">
+		<div className="grow px-4 [&_p]:my-8 [&_hr]:border-1 [&_hr]:border-black [&_li]:list-disc [&_li]:mx-8 [&_h1]:text-2xl [&_h1]:font-bold">
 			{children}
 		</div>
 	);
